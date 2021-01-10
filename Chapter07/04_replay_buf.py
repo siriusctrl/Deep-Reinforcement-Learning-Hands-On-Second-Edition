b301f92d55080168931190be55ad1a0111bab18c
Xinyao Niu <morryniu123@gmail.com>
2021-01-10 23:53:49 +1100

    add comments

--- Chapter07/04_replay_buf.py
+++ Chapter07/04_replay_buf.py
@@ -50,10 +50,13 @@ if __name__ == "__main__":
     buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=100)
 
     for step in range(6):
+        # get one sample from the exp_source and put it into the buffer
         buffer.populate(1)
         # if buffer is small enough, do nothing
         if len(buffer) < 5:
             continue
+
+        # uniformly sample 4 piece of data from the buffer
         batch = buffer.sample(4)
         print("Train time, %d batch samples:" % len(batch))
         for s in batch:
